4e37d5cb7ff8c5c141adae753f166ab56d073e0c
elicia8 <167668883+elicia8@users.noreply.github.com>
2025-06-24 23:23:27 +0700

    add pop up function

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ const navLinks = document.querySelectorAll('nav a').forEach(link => {
         link.classList.add('active');
     }
 })
+
+let popup = document.getElementById("pop-up");
+function open_pop_up(){
+    popup.classList.add("when-pop-up");
+}
+function close_pop_up(){
+    popup.classList.remove("when-pop-up");
+}
